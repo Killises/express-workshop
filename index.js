57f6fe9aa5730894780aec31e2da927f0872a67c
Ulises Cabañas <ulises@MacBook-Air-de-Ulises.local>
2022-02-23 16:34:28 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Bienvenido");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running...")
+});

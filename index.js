6aaf7e2d969ad28bc560aa78333c52507df45666
Ulises <ulicama@hotmail.com>
2022-03-26 01:10:40 -0600

    Added user.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon')
+const user = require('./routes/user')
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) => {
     return res.status(404).json({ code: 404, message: "URL no encontrada" });

0d148c2aead0c2312d92e7d688ed5421a72967d8
Ulises <ulicama@hotmail.com>
2022-04-07 18:02:50 -0500

    conected Signin

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
 const user = express.Router();
 const db = require('../config/database');
 const jwt = require('jsonwebtoken');
 
-user.post("/singin", async(req, res, next) => {
+user.post("/signin", async(req, res, next) => {
     const { user_name, user_mail, user_password } = req.body
 
     if (user_name && user_mail && user_password) {
-        let query = "INSERT INTO user (user_name, user_mail, user_password) "
+        let query = "INSERT INTO user (user_name, user_mail, user_password) ";
         query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`;
         const rows = await db.query(query);
 
         if (rows.affectedRows == 1) {
-            return res.status(201).json({ code: 201, message: "Usuario registrado correctamente" })
+            return res.status(201).json({ code: 201, message: "Usuario registrado correctamente" });
         }
         return res.status(500).json({ code: 500, message: "Ocurrio un error" });
     }
